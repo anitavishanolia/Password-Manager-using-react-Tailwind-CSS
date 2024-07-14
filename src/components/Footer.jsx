f913db3072cc2a4ee478c151fe47827a867fbd11
anitavishanolia <anitas.cs.@nitj.ac.in>
2024-07-14 15:50:05 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-800 text-white flex flex-col justify-center items-center w-full'>
+         <div className="logo font-bold text-2xl">
+        
+        <span className="text-green-500">&lt;</span>
+         <span>Pass</span>
+        <span className="text-green-500">OP/&gt;</span>
+        </div>
+        <div className='flex justify-center items-center'>
+        Created with <img className='w-6 mx-2' src="icons/heart.png" alt="" /> by anita vishanolia
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
